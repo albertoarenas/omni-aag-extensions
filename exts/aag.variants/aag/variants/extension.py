288ce3b88bdd66175dabf654634016ae48f20640
alberto.arenas.garcia <alberto.arenas.garcia@gmail.com>
2023-01-13 00:12:25 +0100

    refac: move extension code under src folder feat: reimplemented create_variant usecase

--- exts/aag.variants/aag/variants/extension.py
+++ exts/aag.variants/aag/variants/extension.py
@@ -3,14 +3,14 @@ from pathlib import Path
 import omni.ext
 import omni.ui as ui
 
-from .create_variant import CreateVariantFromSelection
+from .src.create_variant import CreateVariantFromSelection
 
 VARIANTS_PNG = Path(__file__).parent.parent.parent / "data" / "variants.png"
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
 def some_public_function(x: int):
     print("[aag.reviewnotes] some_public_function was called with x: ", x)
-    return x ** x
+    return x**x
 
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
@@ -28,19 +28,21 @@ class AagVariantsExtension(omni.ext.IExt):
         self._window = ui.Window("Variants", width=300, height=300)
         with self._window.frame:
             with ui.VStack():
-                
-                ui.Image(str(VARIANTS_PNG),
+
+                ui.Image(
+                    str(VARIANTS_PNG),
                     height=200,
-                    #fill_policy=ui.FillPolicy.PRESERVE_ASPECT_FIT,
+                    # fill_policy=ui.FillPolicy.PRESERVE_ASPECT_FIT,
                     alignment=ui.Alignment.CENTER,
-                    style={'border_radius':10})
+                    style={"border_radius": 10},
+                )
                 ui.Spacer(height=5)
 
                 def on_click_create_variant():
-                    self.create_variant_usecase.create_variant()
+                    self.create_variant_usecase.create_variant_from_selection()
 
                 with ui.VStack():
-                    ui.Button("Create Variant", clicked_fn=on_click_create_variant)
+                    ui.Button("Create Variant", height=40, clicked_fn=lambda: on_click_create_variant())
 
     def on_shutdown(self):
         print("[aag.reviewnotes] aag reviewnotes shutdown")
---
+++ exts/aag.variants/aag/variants/src/create_variant.py
@@ -0,0 +1,106 @@
+import enum
+
+from pxr import Usd, UsdGeom, Gf, Sdf
+import omni.usd
+import omni.kit.notification_manager as nm
+import carb
+
+
+class VariantType(enum.Enum):
+    REFERENCE = 0
+    PAYLOAD = 1
+
+
+class CreateVariantFromSelection:
+    def __init__(self):
+        pass
+
+    def create_variant_from_selection(
+        self,
+        variant_set_name: str = "GeomSet",
+        variant_prim_name: str = "VariantPrim",
+        type: VariantType = VariantType.PAYLOAD,
+    ):
+        context = omni.usd.get_context()
+        stage: Usd.Stage = omni.usd.get_context().get_stage()
+
+        # Get selected prims
+        selected_prims = [stage.GetPrimAtPath(m) for m in context.get_selection().get_selected_prim_paths()]
+        if not selected_prims or len(selected_prims) < 2:
+            carb.log_error("ERROR: Nothing or only one primitive was selected.")
+            return False
+
+        # Create variant prim
+        default_prim_path = stage.GetDefaultPrim().GetPath()
+        variant_prim_path = default_prim_path.AppendPath(variant_prim_name)
+        variant_xform_prim = UsdGeom.Xform.Define(stage, variant_prim_path)
+        variant_prim: Usd.Prim = stage.GetPrimAtPath(variant_prim_path)
+
+        # Create variant sets
+        variant_set = variant_prim.GetVariantSets().AddVariantSet(variant_set_name)
+        for prim in selected_prims:
+            variant_name = prim.GetName()
+            all_payloads = []
+            for prim_spec in prim.GetPrimStack():
+                if prim_spec.hasPayloads:
+                    all_payloads.extend(prim_spec.payloadList.GetAddedOrExplicitItems())
+            payload_path = all_payloads[0].assetPath
+
+            variant_set.AddVariant(variant_name)
+            variant_set.SetVariantSelection(variant_name)
+            with variant_set.GetVariantEditContext():
+                if type == VariantType.REFERENCE:
+                    variant_prim.GetReferences().AddReference(Sdf.Reference(payload_path))
+                if type == VariantType.PAYLOAD:
+                    variant_prim.GetPayloads().AddPayload(Sdf.Payload(payload_path))
+
+    def create_variant2(self):
+
+        self._stage = omni.usd.get_context().get_stage()
+
+        # Get selected prim
+        selected_prim_paths = omni.usd.get_context().get_selection().get_selected_prim_paths()
+        print(selected_prim_paths)
+        if not selected_prim_paths or len(selected_prim_paths) > 1:
+            carb.log_error("ERROR: Nothing was selected or multiple selection.")
+            return
+
+        selected_prim_path = selected_prim_paths[0]
+        print(selected_prim_path)
+        selected_prim = self._stage.GetPrimAtPath(Sdf.Path(selected_prim_path))
+        selected_prim_name = selected_prim.GetName()
+
+        # Get selected prim children list
+        selected_prim_children = selected_prim.GetChildren()
+        print(selected_prim_children)
+
+        # Get payload paths
+        payload_paths = []
+        variant_names = []
+        child_paths = []
+        for child_prim in selected_prim_children:
+            payloads: Usd.Payloads = child_prim.GetPayloads()
+            print(child_prim)
+            ref_and_layers = omni.usd.get_composed_payloads_from_prim(child_prim)
+            # print(ref_and_layers)
+            # print(ref_and_layers[0][0].assetPath)
+            payload_paths.append(ref_and_layers[0][0].assetPath)
+            variant_names.append(child_prim.GetName())
+
+            child_paths.append(child_prim.GetPath())
+
+        # Delete children
+        omni.kit.commands.execute("DeletePrims", paths=child_paths, destructive=False)
+
+        # Add a variant set
+        variantset_name = selected_prim_name
+        varset = selected_prim.GetVariantSets().AddVariantSet(variantset_name)
+
+        for i, payload_path in enumerate(payload_paths):
+            variant_name = variant_names[i]
+            varset.AddVariant(variant_name)
+            varset.SetVariantSelection(variant_name)
+            with varset.GetVariantEditContext():
+                selected_prim.GetPayloads().AddPayload(Sdf.Payload(payload_path))
+
+        varset.SetVariantSelection(variant_names[0])
